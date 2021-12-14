36a52c4b3a367a221ca0f99d703ac0e1c9dc21cc
Martynenko <mart902990@gmail.com>
2021-12-14 19:23:52 +0700

    console.log(value);

--- web/form_ajax.js
+++ web/form_ajax.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     }).done(function (result) {
       console.log(result);
     for (let value of result) {
-
+      console.log(value);
         $("#result").html(
             '<p>Полное имя ' + value.result + '</p><p>В лице ' + value.result_genitive + '</p><p>Кому ' + value.result_dative + '</p><p>Согласовано ' + value.result_ablative + '</p>'
           );
